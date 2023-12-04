c286ccf6139badb005b9ec685747c7789d002593
Wellington Jhonney <89779113+Wjhonney@users.noreply.github.com>
2023-12-04 18:18:27 -0300

    correções

--- index.js
+++ index.js
@@ -4,12 +4,15 @@
 const routes = require("./routes/routes_dado.js")
 const express = require("express")
 const cors = require("cors")
+const serverless = require("serverless-http")
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/", routes);
+app.use("/.netlify/functions/api", routes);
 
 app.listen(3000);
+
+module.exports.handler = serverless(app)
